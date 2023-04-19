3817b999402d56f434df5caafda929674e0b534e
Florian Schulz <florian.schulz@hanbei.de>
2023-04-19 22:03:39 +0200

    gilded_rose: test legendary item

--- tests/gildedrose/test_gilded_rose.py
+++ tests/gildedrose/test_gilded_rose.py
@@ -20,6 +20,20 @@ class GildedRoseTest(unittest.TestCase):
         self.assert_item(items[2], sell_in=0, quality=1)
         self.assert_item(items[3], sell_in=0, quality=-1)
 
+    def test_legendary_item_never_loses_quality_and_sell_in(self):
+        items = [
+            Item("Sulfuras, Hand of Ragnaros", 5, 10),
+            Item("Sulfuras, Hand of Ragnaros", 2, 50),
+            Item("Sulfuras, Hand of Ragnaros", 1, 2),
+            Item("Sulfuras, Hand of Ragnaros", -3, -1),
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        self.assert_item(items[0], "Sulfuras, Hand of Ragnaros", sell_in=5, quality=10)
+        self.assert_item(items[1], "Sulfuras, Hand of Ragnaros", sell_in=2, quality=50)
+        self.assert_item(items[2], "Sulfuras, Hand of Ragnaros", sell_in=1, quality=2)
+        self.assert_item(items[3], "Sulfuras, Hand of Ragnaros", sell_in=-3, quality=-1)
 
     def assert_item(self, item, name="foo", sell_in=-1, quality=0):
         self.assertEquals(name, item.name)
