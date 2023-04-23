530a5e24745243ba5a25583d1087262e24807418
Florian Schulz <florian.schulz@hanbei.de>
2023-04-23 22:12:00 +0200

    gilded_rose2: extract hand

--- katas/gildedrose2.py
+++ katas/gildedrose2.py
@@ -16,30 +16,7 @@ class GildedRose(object):
             self._increase_quality(item)
 
     def _update_hand(self, item):
-        if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
-            if item.quality > 0:
-                if item.name != HAND:
-                    item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.name == BACKSTAGE_PASSES:
-                    if item.sell_in < 11:
-                        self._increase_quality(item)
-                    if item.sell_in < 6:
-                        self._increase_quality(item)
-        if item.name != HAND:
-            self._update_sell_in(item)
-        if item.sell_in < 0:
-            if item.name != AGED_BRIE:
-                if item.name != BACKSTAGE_PASSES:
-                    if item.quality > 0:
-                        if item.name != HAND:
-                            item.quality = item.quality - 1
-                else:
-                    item.quality = item.quality - item.quality
-            else:
-                self._increase_quality(item)
+        pass
 
     def _update_backstage_passes(self, item):
         if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
