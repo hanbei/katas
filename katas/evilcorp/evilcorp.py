ca9fd7ab390de3ad027f8530435f9fc4ae9fa56e
Florian Schulz <florian.schulz@hanbei.de>
2023-06-20 21:06:35 +0200

    evil: second iteration

--- katas/evilcorp/evilcorp.py
+++ katas/evilcorp/evilcorp.py
@@ -1,2 +1,9 @@
 def censor(text: str) -> str:
-    return text.replace("nice", "XXXX")
+    return censor_multiple_words(text, ["nice"])
+
+
+def censor_multiple_words(text: str, words: list[str] = None) -> str:
+    result = text
+    for word in words or []:
+        result = result.replace(word, "X" * len(word))
+    return result
--- tests/test_evilcorp.py
+++ tests/test_evilcorp.py
@@ -4,11 +4,30 @@ from katas import evilcorp
 def test_censor_empty():
     assert evilcorp.censor("") == ""
 
+
 def test_censor_nice_as_word():
     assert evilcorp.censor("You are a nice person") == "You are a XXXX person"
+
 
 def test_censor_nice_not_as_word():
     assert evilcorp.censor("You are a niceperson") == "You are a XXXXperson"
 
+
 def test_censor_nice_different_Case():
     assert evilcorp.censor("We are going to Nice") == "We are going to Nice"
+
+
+def test_censor_multiple_words():
+    assert evilcorp.censor_multiple_words("Such a nice day with a bright sun, makes me happy",
+                                          ["nice", "pony", "sun", "light", "fun", "happy", "funny",
+                                           "joy"]) == "Such a XXXX day with a bright XXX, makes me XXXXX"
+
+
+def test_censor_multiple_words_empty_list():
+    assert evilcorp.censor_multiple_words("Such a nice day with a bright sun, makes me happy",
+                                          []) == "Such a nice day with a bright sun, makes me happy"
+
+
+def test_censor_multiple_words_none_list():
+    assert evilcorp.censor_multiple_words("Such a nice day with a bright sun, makes me happy",
+                                          None) == "Such a nice day with a bright sun, makes me happy"
